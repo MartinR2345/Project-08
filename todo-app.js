982257f9d1c29fa9d32a5ce69b151f51ca88cd32
Martin <94710502+MartinR2345@users.noreply.github.com>
2022-10-10 14:56:33 -0400

    I made a couple of changes to the code and comments but I was able to get it to work

--- todo-app.js
+++ todo-app.js
@@ -1,12 +1,12 @@
 // From line 2 to line 3, I grabbing from the DOM the elements I want to use with the document.querySelector() and saving them to variables
-const todoItems = document.querySelector('.todo-items');
-const todoForm = document.querySelector('.todo-form');
+const todoItems = document.querySelector('#todos')
+const todoForm = document.querySelector('#todo-form')
 
 
 // On line 7, I'm fetch any existing "todos" from localStorage by checking to see if the JSON data actually exists by seeing if it's not equal to null
 const getFromLocalStorage = () => {
-    const todosJSON = localStorage.getItem("todos")
-    return todosJSON !== null ? JSON.parse(todosJSON) : [];
+    const todosJSON = localStorage.getItem('todos')
+    return todosJSON !== null ? JSON.parse(todosJSON) : [] 
 }
 
 
@@ -15,7 +15,7 @@ let todos = getFromLocalStorage();
 
 
 // On line 18, I'm adding/saving the todos to the localStorage
-const addToLocalStorage(todos) => {
+const addToLocalStorage = (todos) => {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
@@ -46,3 +46,128 @@ todoForm.addEventListener('submit', function (e) {
     }
 })
 
+// On line 50, I made a filter's object.  This is going to keep track of the latest data.  This changes as the "searchText" search input changes down at the very bottom.
+const filters = {
+    searchText: ''
+}
+
+// From line 55 to line 99, (A-1) Get the DOM elements for an individual note.  This will generate the todo DOM element by taking the todo data and creating individual paragraphs with checkboxes and remove buttons.  This generates the todo element, which makes up the little element we show for each todo, that is the checkbox, our text and our button.
+const generateTodoDOM = (todo) => {
+    //Setup a root element (div) 
+    const todoElement = document.createElement('div');
+
+    //Create checkbox
+    const checkbox = document.createElement('input')
+    // Setup todo checkbox 
+    checkbox.setAttribute('type', 'checkbox')
+    //(C-1) PART 1 - 
+    checkbox.checked = todo.completed
+    todoElement.appendChild(checkbox)
+    //(C-1) Part 2 - add an eventListener on checkbox that listens for a change event basically gets fire when the checkbox gets checked or unchecked
+    checkbox.addEventListener('change', function () {
+        //Call "toggleTodo" - Toggle the todo by id
+        toggleTodo(todo.id)
+        //Calt addToLocalStorage which is passing in my list 
+         addToLocalStorage(todos)
+        //Call renderTodos which will render this new list with the one item removed 
+        renderTodos(todos, filters)
+    })
+
+    //Create todoText
+    const todoText = document.createElement('span')
+     //Setup the todo text by changing the text content for that span with textContent then appending the text element to the div
+     todoText.textContent = todo.text
+     todoElement.appendChild(todoText)
+
+
+    //Setup the removeButton
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'x'
+    todoElement.appendChild(removeButton)
+    //(R-1) - removeButton part 1
+    removeButton.addEventListener('click', function(){
+        //Call to removeTodo which I'm passing todo.id that comes from the parent scope "todo" in generateTodoDOM
+        removeTodo(todo.id)
+        //Call tt addToLocalStorage which is passing in my list 
+        addToLocalStorage(todos)
+        //Call to renderTodos which will render this new list with the one item removed 
+        renderTodos(todos, filters)
+    })
+
+    //I'm returning todoElement from this function
+    return todoElement 
+}
+
+
+// On line 103, (A-2) Get the DOM elements for list summary.  This function "generateSummary" is creating an h2 element and saving it to a variable called "summmary" and using the textContent to write a text then I'm returning that summary
+const generateSummaryDOM = (incompleteTodos) => {
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    return summary
+}
+
+
+// On line 111, I'm rendering/displaying the todos application on the screen with this renderTodos function
+const renderTodos = function (todos, filters) {   
+    //This line of code basically clears the div before rendering anything into it otherwise I’m going to get duplicate data.
+    todoItems.innerHTML = ''
+
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    //This line of code is looking at the filtered list.  This is going to make sure to only count the incomplete todos in the ones that we’re actually able to see.
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+
+    //(A-2)This is rendering or displaying the incompleted todos to the web browser by creating an h2 tag and it appends it to the div with the ID of todos
+    todoItems.appendChild(generateSummaryDOM(incompleteTodos))
+
+
+    //(A-1)This is rendering or displaying the individual paragraphs by appending my function "generateTodoDOM"
+    //This if statement basically is stating if there are todos to show, then render them 
+    //The else statement basically is stating if there are no todos to show then render a paragraph instead and add a class empty message and some text message like "No to-dos to show"
+    if (filteredTodos.length > 0){
+        filteredTodos.forEach(todo => {
+            todoItems.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'No to-dos to show'
+        todoItems.appendChild(messageEl);
+    }
+}
+
+// On line 145, I'm calling the function renderTodos
+renderTodos(todos, filters)
+
+
+// On line 149, (R-1) removeButton PT2 (I made a "removeTodo" function that removes todo by ID)
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex(todo => {
+        return todo.id === id
+    })
+
+    //This finds out if we have a match
+    if (todoIndex > -1 ){
+        //I'm passing in the todoIndex and the number of things I want to remove which is 1
+        todos.splice(todoIndex, 1)
+    }
+}
+
+// On line 163, (C-1) PT2 (I created a function called "toggleTodo" and this will toggle the completed value for a given todo)
+const toggleTodo = (id) => {
+    //I'm using the find() method to find the object and return the actually match 
+    const todo = todos.find(todo => {
+        //Return true when it finds a match
+        return todo.id === id
+    })
+
+    //Now there is a chance if it doesn't find a match then I want to modified it by getting the current value, flipping it and then store that as the new value.
+  if (todo) {
+        todo.completed = !todo.completed
+    }
+}
